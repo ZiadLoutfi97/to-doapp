ac80f9b0e5aa4b51b2819c661b9dbcfe39922102
Ziad Loutfi <ozoz6661@gmail.com>
2023-08-28 00:29:58 +0300

    first commit

---
+++ to-do-app/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import './styles.css'
+export default function App() {
+  const [newItem,setNewItem] = useState("")
+  const [todos,setTodos]= useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodos(currentTodos => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title: newItem, completed: false },
+    ]);
+  setNewItem("")
+    }
+    function toggleTodo(id,completed){
+setTodos(currentTodos => {
+  return currentTodos.map( todo => {
+if(todo.id===id) {
+  return {...todo, completed}
+}
+return todo
+  })
+})
+    }
+function deleteTodo(id) {
+  setTodos(currentTodos =>{
+    return currentTodos.filter(todo => todo.id !== id)
+  })
+}
+return <>
+<form className="new-item-form" onSubmit={handleSubmit}>
+  <div className="form-row">
+    <label htmlFor="item">New Item</label>
+    <input 
+    type="text"
+     id="item" 
+    value={newItem} 
+    onChange={e => setNewItem(e.target.value)}></input>
+  </div>
+  <button className="btn">Add</button>
+</form>
+<h1 className="header">To do list</h1>
+<ul className='list'>
+{todos.map(todo =>{
+  return <li key={todo.id}>
+    <label>
+      <input type={todo.completed.checked} onChange={e => toggleTodo(todo.id,e.target.checked)}/>
+        {todo.title}
+    </label>
+    <button className="btn btn-danger" onClick={()=>deleteTodo(todo.id)}>Delete</button>
+  </li>
+})}
+</ul>
+</>
+}
